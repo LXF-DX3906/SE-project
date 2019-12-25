cfc601b473bddae1d9233dda2f7a79956efb7670
LXF-DX3906 <1621413639@qq.com>
2019-12-25 18:55:25 +0800

    vue.config.js

---
+++ client-vue/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    devServer: {
+        open: true,
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8021',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+    productionSourceMap:false,
+};
